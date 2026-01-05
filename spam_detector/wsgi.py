e391de5d38b39b33982501d39ed138d40f821055
dandark666 <82452568+dandark666@users.noreply.github.com>
2026-01-05 14:49:43 -0600

    Update wsgi.py

--- spam_detector/wsgi.py
+++ spam_detector/wsgi.py
@@ -4,13 +4,14 @@ WSGI config for spam_detector project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spam_detector.settings')
+# Usar settings de producción por defecto
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spam_detector.settings_prod')
 
 application = get_wsgi_application()
